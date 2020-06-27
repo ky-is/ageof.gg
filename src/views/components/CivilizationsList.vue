7fc6e236529da83c32f3363736137431e181c519
Kyle Coburn <kylecoburn@gmail.com>
2020-06-27 00:51:32 -0700

    ✨ show civ list in sidebar

--- src/views/components/CivilizationsList.vue
+++ src/views/components/CivilizationsList.vue
@@ -1,12 +1,25 @@
 <template>
 	<div class="w-64 h-full bg-gray-800">
 		<h2>Civilizations</h2>
+		<div v-for="civilization in civBonuses" :key="civilization.name" class="my-1 flex items-center">
+			<img :src="`src/assets/images/civs/${civilization.name.toLowerCase()}.png`" class="w-12 mr-1">
+			<div class="leading-tight">
+				<h3 class="text-lg">{{ civilization.name }}</h3>
+				<div class="text-sm">{{ civilization.focuses.join(' + ') }}</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script lang="ts">
+import civBonuses from '../../models/civs/bonuses'
+
 export default {
 	components: {
+	},
+
+	setup () {
+		return { civBonuses }
 	},
 }
 </script>
