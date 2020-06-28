30bb477beb6d79cbaef6ca72ab5332a33b1b018b
Kyle Coburn <kylecoburn@gmail.com>
2020-06-27 22:30:18 -0700

    ♻️ civ icon

---
+++ src/views/components/CivIcon.vue
@@ -0,0 +1,18 @@
+<template>
+	<img :src="`/images/civs/${civ.name.toLowerCase()}.png`" class="inline-block">
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import { CivilizationBonusesEntry } from '/@/models/civs/bonuses'
+
+export default defineComponent({
+	props: {
+		civ: {
+			type: Object as () => CivilizationBonusesEntry,
+			required: true,
+		},
+	},
+})
+</script>
--- src/views/pages/CompBuilder/CivList/FilterList.vue
+++ src/views/pages/CompBuilder/CivList/FilterList.vue
@@ -2,7 +2,7 @@
 	<div class="mt-1">
 		<h3 class="text-lg text-secondary smallcaps">{{ header }}</h3>
 		<div v-for="civilization in civs" :key="civilization.name" class="my-2  flex items-start">
-			<img :src="`/images/civs/${civilization.name.toLowerCase()}.png`" class="w-10 -ml-1 mr-1">
+			<CivIcon :civ="civ" class="w-10 -ml-1 mr-1" />
 			<div class="leading-tight">
 				<h3 class="text-lg">{{ civilization.name }}</h3>
 				<div class="text-sm text-secondary">{{ civilization.focuses.join(' + ') }}</div>
@@ -14,9 +14,15 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import CivIcon from '/@/views/components/CivIcon.vue'
+
 import { CivilizationBonusesEntry } from '/@/models/civs/bonuses'
 
 export default defineComponent({
+	components: {
+		CivIcon,
+	},
+
 	props: {
 		header: {
 			type: String,
