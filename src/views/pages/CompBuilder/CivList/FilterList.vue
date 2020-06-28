98c28a7feb584c8067ce57d23a8cfc0d58569bbc
Kyle Coburn <kylecoburn@gmail.com>
2020-06-27 20:49:06 -0700

    🐛 dynamic civ icon paths

--- src/views/pages/CompBuilder/CivList/FilterList.vue
+++ src/views/pages/CompBuilder/CivList/FilterList.vue
@@ -2,7 +2,7 @@
 	<div class="mt-1">
 		<h3 class="text-lg text-secondary smallcaps">{{ header }}</h3>
 		<div v-for="civilization in civs" :key="civilization.name" class="my-2  flex items-start">
-			<img :src="`src/assets/images/civs/${civilization.name.toLowerCase()}.png`" class="w-10 -ml-1 mr-1">
+			<img :src="`/images/civs/${civilization.name.toLowerCase()}.png`" class="w-10 -ml-1 mr-1">
 			<div class="leading-tight">
 				<h3 class="text-lg">{{ civilization.name }}</h3>
 				<div class="text-sm text-secondary">{{ civilization.focuses.join(' + ') }}</div>
