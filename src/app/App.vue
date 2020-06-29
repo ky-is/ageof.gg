e452d03bcbc43e08155001dea74cfa7f8b44659f
Kyle Coburn <kylecoburn@gmail.com>
2020-06-28 20:38:35 -0700

    🐛💄 fill exact page height

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,10 +1,12 @@
 <template>
-	<header class="h-12 px-4 bg-gray-900 text-white  flex items-center">
-		<h1 class="-mt-1 text-2xl font-thin ">ageof.gg</h1>
-	</header>
-	<main class="h-full text-gray-100">
-		<CompBuilder />
-	</main>
+	<div class="h-full  flex flex-col">
+		<header class="h-12 px-4 bg-gray-900 text-white  flex items-center">
+			<h1 class="-mt-1 text-2xl font-thin ">ageof.gg</h1>
+		</header>
+		<main class="text-gray-100 flex-grow">
+			<CompBuilder />
+		</main>
+	</div>
 </template>
 
 <script lang="ts">
