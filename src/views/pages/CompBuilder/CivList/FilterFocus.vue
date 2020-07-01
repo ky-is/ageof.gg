b1ef92887a03f2a8d962d8ba4c4be83a85b8f6b2
Kyle Coburn <kylecoburn@gmail.com>
2020-06-30 17:08:46 -0700

    ♿️ select label

--- src/views/pages/CompBuilder/CivList/FilterFocus.vue
+++ src/views/pages/CompBuilder/CivList/FilterFocus.vue
@@ -1,14 +1,17 @@
 <template>
-	<select v-model="value" class="ui-select -mx-px" name="Filter focus">
-		<option value="" disabled class="ui-option">Filter by:</option>
-		<template v-if="selected">
-			<option value="" class="ui-option text-secondary">None</option>
-			<option disabled class="ui-option">—</option>
-		</template>
-		<option v-for="[key, focus] in focusFilters" :key="key" :value="key" class="ui-option">
-			{{ focus }}
-		</option>
-	</select>
+	<div>
+		<label for="filterCivs" class="hidden">Filter civs:</label>
+		<select id="filterCivs" v-model="value" class="ui-select -mx-px" name="Filter focus">
+			<option value="" disabled class="ui-option">Filter by:</option>
+			<template v-if="selected">
+				<option value="" class="ui-option text-secondary">None</option>
+				<option disabled class="ui-option">—</option>
+			</template>
+			<option v-for="[key, focus] in focusFilters" :key="key" :value="key" class="ui-option">
+				{{ focus }}
+			</option>
+		</select>
+	</div>
 </template>
 
 <script>
