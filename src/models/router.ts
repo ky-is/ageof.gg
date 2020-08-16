6df66d01adc325915d10331e1d0b4d36369a10d8
Kyle Coburn <kylecoburn@gmail.com>
2020-08-16 15:40:27 -0600

    ♻️ nest game routes

--- src/models/router.ts
+++ src/models/router.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import RouterParent from '/@/views/components/RouterParent.vue'
+
 import CompBuilder from '/@/views/pages/CompBuilder/index.vue'
 import GamesPage from '/@/views/pages/Games/index.vue'
 import GameCivIcons from '/@/views/pages/Games/CivIcons.vue'
@@ -18,20 +20,26 @@ const router = createRouter({
 
 		{
 			path: '/games',
-			component: GamesPage,
+			component: RouterParent,
+			children: [
+				{
+					path: '',
+					component: GamesPage,
+				},
+				{
+					path: 'civ-icons',
+					component: GameCivIcons,
+				},
+				// {
+				// 	path: '/games/civ-sounds',
+				// 	component: GamesPage,
+				// },
+				// {
+				// 	path: '/games/taunts',
+				// 	component: GamesPage,
+				// },
+			],
 		},
-		{
-			path: '/games/civ-icons',
-			component: GameCivIcons,
-		},
-		// {
-		// 	path: '/games/civ-sounds',
-		// 	component: GamesPage,
-		// },
-		// {
-		// 	path: '/games/taunts',
-		// 	component: GamesPage,
-		// },
 	],
 })
 
