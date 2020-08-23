4f8fb4c830fe3ad072f3979639b09d198e44e2b7
Kyle Coburn <kylecoburn@gmail.com>
2020-08-23 10:38:24 -0600

    ✨ games dropdown menu

--- src/views/pages/Games/index.vue
+++ src/views/pages/Games/index.vue
@@ -2,9 +2,11 @@
 	<div class="h-content bg-gray-850">
 		<UIStack direction="col" class="max-w-xl mx-auto px-4">
 			<h2 class="my-4 text-3xl font-light">Games</h2>
-			<h3 class="text-xl font-light"><RouterLink to="/games/civ-icons">Civ icon trainer</RouterLink></h3>
-			<!-- <h3 class="text-xl font-light"><RouterLink to="/games/civ-sounds">Guess the civ sound effect</RouterLink></h3> -->
-			<!-- <h3 class="text-xl font-light"><RouterLink to="/games/taunts">Guess the taunt</RouterLink></h3> -->
+			<div>
+				<h3 class="text-xl font-light leading-loose"><RouterLink to="/games/civ-icons">Civ icon trainer</RouterLink></h3>
+				<h3 class="text-xl font-light leading-loose text-secondary">(coming soon) Civ Sounds</h3><!-- <RouterLink to="/games/civ-sounds">Guess the civ sound effect</RouterLink> -->
+				<h3 class="text-xl font-light leading-loose text-secondary">(coming soon) Taunts</h3><!-- <RouterLink to="/games/taunts">Guess the taunt</RouterLink> -->
+			</div>
 		</UIStack>
 	</div>
 </template>
