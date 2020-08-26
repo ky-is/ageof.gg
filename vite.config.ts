e4147706e12041524f484d141c7d51904d909691
Kyle Coburn <kylecoburn@gmail.com>
2020-08-26 10:28:51 -0600

    ⬆️

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,19 @@ import path from 'path'
 
 import ViteComponentsImport from 'vite-plugin-components'
 
+const alias = {
+	'/@/': path.resolve(__dirname, 'src'),
+}
+
 export default {
-	alias: {
-		'/@/': path.resolve(__dirname, 'src'),
-	},
+	alias,
 	define: {
 		__VUE_OPTIONS_API__: false,
 	},
 	plugins: [
 		ViteComponentsImport({
 			dirs: ['src/views'],
+			alias,
 		}),
 	],
 }
